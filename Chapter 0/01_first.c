6907b941cf8c8c0620f93403760daa53b5b1507a
satvikcodes <satvikcodes>
2024-07-23 13:47:11 +0530

    chapter 0 completed

---
+++ Chapter 0/01_first.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // Pre Processor Directive (Header File)
+
+int main()
+{
+    printf("Om Shree Gadeshae Namaha");
+    return 0;
+    // This program first written by - Brian Khernighan at BPCL
+}
+
+// Boiler Plate Code - Likhna he likhna hai
